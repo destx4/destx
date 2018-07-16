c389f8ed8cb9a02a599963f0caa7219558b32604
kjcohen <38214257+kjcohen@users.noreply.github.com>
2018-07-16 08:25:53 -0400

    Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RotateComponent } from './rotate/rotate.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'rotate', component: RotateComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
